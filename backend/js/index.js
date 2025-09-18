97f2e99ae42a8d0ea2b81f0499f38b770fc3d761
IsItLucas? <lucas.galonet.com@gmail.com>
2025-09-17 21:25:33 -0300

    adicionando rotas do backend

--- backend/js/index.js
+++ backend/js/index.js
@@ -120,10 +120,13 @@ export async function obter_tabela(tabela) {
 export async function setup_database() {
 	const conexao = await db.conectar();
 
-	await conexao.execute(`DROP TABLE IF EXISTS endereços`);
-	await conexao.execute(`DROP TABLE IF EXISTS compras`);
-	await conexao.execute(`DROP TABLE IF EXISTS avaliacoes`);
-	await conexao.execute(`DROP TABLE IF EXISTS alunos`);
+	// await conexao.execute(`DROP TABLE IF EXISTS endereços`);
+	// await conexao.execute(`DROP TABLE IF EXISTS compras`);
+	// await conexao.execute(`DROP TABLE IF EXISTS avaliacoes`);
+	// await conexao.execute(`DROP TABLE IF EXISTS alunos`);
+	await conexao.execute(`DROP TABLE IF EXISTS railway`);
+	await conexao.execute(`CREATE TABLE railway`);
+	await conexao.execute(`USE railway`);
 
 	// Tabela alunos
 	await conexao.execute(`
